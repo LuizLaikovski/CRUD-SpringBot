cf821711d79a06c57b8bf29e9b3478bd5025a3db
Luiz Henrique Pasin Laikovski <160736729+LuizLaikovski@users.noreply.github.com>
2026-04-30 07:38:34 -0300

    iniciando o projeto

---
+++ src/main/java/com/createpdf/luizdev/LuizdevApplication.java
@@ -0,0 +1,13 @@
+package com.createpdf.luizdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuizdevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuizdevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/createpdf/luizdev/LuizdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.createpdf.luizdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuizdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
